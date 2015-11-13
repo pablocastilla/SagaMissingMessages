cb2ec020a309b6641091fb832c44db454866c0e4
pablocastilla <pablo.castilla@gmail.com>
2015-11-13 14:56:24 +0100

    initial commit

---
+++ SagaMissingMessages/SagaMissingMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Saga;
+
+namespace SagaMissingMessages
+{
+    public class SagaMissingMessages : Saga<SagaMissingMessagesSagaData>, IAmStartedByMessages<InitSagaCommand>, IHandleMessages<ValidateSomethingReply>
+    {
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<SagaMissingMessagesSagaData> mapper)
+        {
+
+     
+        }
+
+        public void Handle(InitSagaCommand message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ValidateSomethingReply message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SagaMissingMessages/SagaMissingMessagesSagaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Saga;
+
+namespace SagaMissingMessages
+{
+    public class SagaMissingMessagesSagaData : ContainSagaData
+    {
+    }
+}
